aace08834a8a7547463a9e7954ffcea8caa4a3bf
Arkadiusz Czerwiński <42278982+arkadiusz-czerwinski@users.noreply.github.com>
2021-08-18 20:54:03 +0200

    fixes

--- run.py
+++ run.py
@@ -1,45 +1,10 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import os
-import numpy as np
-import matplotlib.pyplot as plt
 
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='local')
 tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
 strategy = tf.distribute.TPUStrategy(cluster_resolver)
 
-labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
-imagenet_labels = np.array(open(labels_path).read().splitlines())
-
-# Set data_dir to a read-only storage of .tar files
-# Set write_dir to a w/r storage
-data_dir = 'datasets/imagenet/'
-write_dir = 'psando/tf-imagenet-dirs'
-
-# Construct a tf.data.Dataset
-download_config = tfds.download.DownloadConfig(
-                      extract_dir=os.path.join(write_dir, 'extracted'),
-                      manual_dir=data_dir
-                  )
-download_and_prepare_kwargs = {
-    'download_dir': os.path.join(write_dir, 'downloaded'),
-    'download_config': download_config,
-}
-ds = tfds.load('imagenet2012_subset', 
-               data_dir=os.path.join(write_dir, 'data'),         
-               split='train', 
-               shuffle_files=False, 
-               download=True, 
-               as_supervised=True,
-               download_and_prepare_kwargs=download_and_prepare_kwargs)
-               
-               
-def resize_with_crop(image, label):
-    i = image
-    i = tf.cast(i, tf.float32)
-    i = tf.image.resize_with_crop_or_pad(i, 224, 224)
-    i = tf.keras.applications.mobilenet_v2.preprocess_input(i)
-    return (i, label)
-
-# Preprocess the images
-ds = ds.map(resize_with_crop)
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
